f8ad2db8d05e1fa54d81a3ce9d30b5121f061724
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2022-05-17 23:59:59 +0900

    add: Layout Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Mario Plan</h1>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/layout/SignedInLinks.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const SignedInLinks = () => {
+    return (
+            <ul className="right">
+            <li><NavLink to='/'>New Project</NavLink></li>
+            <li><NavLink to='/'>Log Out</NavLink></li>
+            {/* // 作業段階にて下の行で'NN'を忘れたので下画像で円内に'NN'が表示されてませんが */}
+            {/* // 表示されているのが正常なので悪しからず． */}
+            <li><NavLink to='/' className="btn btn-floating pink lighten-1">NN</NavLink></li>
+        </ul>
+    )
+}
+
+export default SignedInLinks;
