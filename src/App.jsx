95bcafa325c87c565404e75451aa565cf5613abe
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2022-05-18 01:19:05 +0900

    add: SignIn & SignUp Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
-import Dashboard from './components/dashboard/Dashboard'
-import ProjectDetails from './components/projects/ProjectDetails'
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+
 
 class App extends Component {
   render() {
@@ -13,6 +16,8 @@ class App extends Component {
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path='/project/:id' element={<ProjectDetails />} />
+            <Route path='/signin' element={<SignIn />} />
+            <Route path='/signup' element={<SignUp />} />
           </Routes>
         </div>
       </BrowserRouter>
