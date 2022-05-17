616180018c00b01984e737ee4acba981f6b2f1ed
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2022-05-18 01:14:26 +0900

    add: ProjectDetails Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
+import ProjectDetails from './components/projects/ProjectDetails'
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <Navbar />
           <Routes>
             <Route path="/" element={<Dashboard />} />
+            <Route path='/project/:id' element={<ProjectDetails />} />
           </Routes>
         </div>
       </BrowserRouter>
--- src/components/dashboard/Notification.jsx
+++ src/components/dashboard/Notification.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Notification = () => {
     return (
         <div>
-　　　　　　　// 通知の表示はfirebaseとの連携が必要なので結構後半で構うまでは適当にpタグで我慢．．．
+{/* 　　　　　　　// 通知の表示はfirebaseとの連携が必要なので結構後半で構うまでは適当にpタグで我慢．．． */}
             <p>Notification</p>
         </div>
     )
